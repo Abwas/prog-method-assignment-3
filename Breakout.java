0b3fd55961056f6bb75d41385b95385d3364e981
S <s>
2017-06-07 06:36:10 +0100

    {"types":"C","runs":40}

--- Breakout.java
+++ Breakout.java
@@ -66,7 +66,7 @@ public class Breakout extends GraphicsProgram {
 		
 		double x = 0.5;
 		
-		double y = getHeight() - (BRICK_HEIGHT + BRICK_Y_OFFSET);
+		double y = getHeight() - (BRICK_HEIGHT + BRICK_Y_OFFSET) / 9;
 		
 		for (int row = 0; row < NBRICK_ROWS; row++) {
 			
