52fde190405dddd8dbde05d582a8e8850debfb6b
S <s>
2017-06-13 05:17:13 +0100

    {"types":"C","runs":121}

--- Breakout.java
+++ Breakout.java
@@ -169,7 +169,7 @@ public class Breakout extends GraphicsProgram {
 		private void getBallVelocity(){
 	        vx = rgen.nextDouble(1.0, 3.0);
 	        if (rgen.nextBoolean(0.5)) vx = - vx;
-	        vy = +5.0;
+	        vy = 5.0;
 	    }
 	    // makes the ball move
 	    private void moveBall(){
